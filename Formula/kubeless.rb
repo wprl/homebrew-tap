c9c690b7ef674944bc94b0eb40487bcbd22e1a48
Sebastien Goasguen <runseb@gmail.com>
2017-07-04 19:26:11 +0200

    bump to 0.0.15

--- Formula/kubeless.rb
+++ Formula/kubeless.rb
@@ -1,10 +1,10 @@
 class Kubeless < Formula
   desc "Kubernetes Native Serverless Framework"
   homepage "https://github.com/kubeless"
-  version "0.0.13"
+  version "0.0.15"
 
-  url "https://github.com/kubeless/kubeless/releases/download/0.0.13/kubeless_darwin-amd64.zip"
-  sha256 "407c2b211ee244ffad4a156ef148165087726d82829587fd6e5e908f21d9807f"
+  url "https://github.com/kubeless/kubeless/releases/download/0.0.15/kubeless_darwin-amd64.zip"
+  sha256 "bb1aa1dec0da9dc9f746869ac4f5d52db794f0e7cfea29d99dbf3190871b874a"
 
   bottle :unneeded
 
@@ -15,6 +15,8 @@ class Kubeless < Formula
   def caveats; <<-EOS.undent
     To use kubeless you need a running Kubernetes cluster
     Try `kubectl version` to make sure you are ready
+    Then launch the kubeless controller with:
+    `kubectl create -f https://github.com/kubeless/kubeless/releases/download/0.0.15/kubeless-0.0.15.yaml`
     EOS
   end
 
