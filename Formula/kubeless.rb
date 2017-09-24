538d0bd16dbb7f85e267952badd6f3c1c7272cb9
Ara Pulido <apulido@gmail.com>
2017-09-24 18:56:45 +0200

    Updated formula for version 0.2.0

--- Formula/kubeless.rb
+++ Formula/kubeless.rb
@@ -1,10 +1,10 @@
 class Kubeless < Formula
   desc "Kubernetes Native Serverless Framework"
   homepage "https://github.com/kubeless"
-  version "v0.1.0"
+  version "v0.2.0"
 
-  url "https://github.com/kubeless/kubeless/releases/download/v0.1.0/kubeless_darwin-amd64.zip"
-  sha256 "8d1eadfc5e519048da801d26117aee9e84c07cc3a069787f89bca519f8a0bd6f"
+  url "https://github.com/kubeless/kubeless/releases/download/v0.2.0/kubeless_darwin-amd64.zip"
+  sha256 "d757c14d6b112441dee1ab7e283777b19f42825d9e7adb76b684cb2d24218277"
 
   bottle :unneeded
 
@@ -16,7 +16,7 @@ class Kubeless < Formula
     To use kubeless you need a running Kubernetes cluster
     Try `kubectl version` to make sure you are ready
     Then launch the kubeless controller with:
-    `curl -sL https://github.com/kubeless/kubeless/releases/download/v0.1.0/kubeless-v0.1.0.yaml | kubectl create -f -`
+    `curl -sL https://github.com/kubeless/kubeless/releases/download/v0.2.0/kubeless-v0.2.0.yaml | kubectl create -f -`
     EOS
   end
 
